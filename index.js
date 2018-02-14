85303c4d491ad555d9fc5576fa632ab055abdeed
Tom Burris <unsaneity@gmail.com>
2018-02-14 02:57:30 -0800

    general improvements

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 const resize = () => {
-    iframe.height = Math.min(window.innerHeight, window.innerWidth);
-    iframe.width = iframe.height;
-
-    instructions.style.left = (window.innerWidth - instructions.offsetWidth) / 2 + "px";
-    instructions.style.top = (window.innerHeight - instructions.offsetHeight) / 2 + "px";
+    iframe.width = iframe.height = Math.min(innerHeight, innerWidth);
 };
 const hideInstructions = () => {
     instructions.style.display = "none";
-    if (iframe.focus) {
-        iframe.focus();
-    } else if (iframe.contentWindow) {
-        iframe.contentWindow.focus();
-    } else if (iframe.contentDocument && iframe.contentDocument.documentElement) {
-        // For old versions of Safari
-        iframe.contentDocument.documentElement.focus();
-    }
+    iframe.focus();
 };
-
-let iframe;
-let instructions;
-
-document.addEventListener("DOMContentLoaded", () => {
+const init = () => {
     iframe = document.getElementById("iframe");
     instructions = document.getElementById("instructions");
     window.addEventListener("resize", () => requestAnimationFrame(resize));
     resize();
     setTimeout(hideInstructions, 5e3);
-});
+};
+
+let iframe;
+let instructions;
+
+document.addEventListener("DOMContentLoaded", init);
--- snake.js
+++ snake.js
@@ -6,7 +6,7 @@ const rainbowMode = true;
 
 // tables
 const directions = [new Point(1, 0), new Point(0, -1), new Point(-1, 0), new Point(0, 1), new Point(0, 0)]; // Right, up, left, down, none.
-const keyCodeToDir = {"37": 2, "38": 1, "39": 0, "40": 3, "68": 0, "87": 1, "65": 2, "83": 3}; // leftArrow, upArrow, rightArrow, downArrow, d, w, a, s.
+const keyCodeToDir = {37: 2, 38: 1, 39: 0, 40: 3, 68: 0, 87: 1, 65: 2, 83: 3}; // leftArrow, upArrow, rightArrow, downArrow, d, w, a, s.
 const dirToKeyCode = [39, 38, 37, 40]; // Right, up, left, down.
 const loseMessages = ["you lost! :(", "ouch!", "oh noes!", "that's a headache", "hasta la vista snakey", "now you're fertilizer", "R.I.P.", "WASTED",
     "*Windows blue screen*", "keep your day job", "blame lag", "why even bother?", "one more!", "so close! not.", "'clutch'", "1-800-273-8255", "go outside"];
@@ -16,12 +16,12 @@ let canvas;
 let ctx;
 let snake;
 let dir;
-let loop;
 let apple;
 let keydown;
 let empty;
 let finalText;
-let main;
+let prev;
+let progress = 0;
 let paused = false;
 let deathCount = 0;
 
@@ -31,7 +31,7 @@ window.addEventListener("resize", () => {
     draw();
 });
 window.addEventListener("keydown", keydown = event => { // Get user input.
-    if (event.preventDefault && [32, 37, 38, 39, 40].indexOf(event.keyCode) != -1) { // stop firefox from scrolling the page.
+    if ([32, 37, 38, 39, 40].indexOf(event.keyCode) != -1) { // stop firefox from scrolling the page.
         event.preventDefault();
     }
     if (event.keyCode == 13 || event.keyCode == 32) {
@@ -39,31 +39,38 @@ window.addEventListener("keydown", keydown = event => { // Get user input.
             finalText = null;
             initSnake();
             newApple();
-            draw();
-            loop = setInterval(main, ~~(1000 / sps));
-        } else if (!paused) {
-            clearInterval(loop);
-            paused = true;
-            draw();
         } else {
-            loop = setInterval(main, ~~(1000 / sps));
-            paused = false;
+            paused = !paused;
         }
         return;
     }
-    const tmpDir = keyCodeToDir[event.keyCode+""]; // Convert keyCode to string, then a direction.
+    const tmpDir = keyCodeToDir[event.keyCode];
     if (tmpDir != null && tmpDir != dir[dir.length - 1] && (((tmpDir + 2) % 4) != dir[dir.length - 1] || snake.length == 1)) {
         dir.push(tmpDir);
     }
 });
-const initCanvas = () => { // Setup canvas with context and dimensions.
+const initCanvas = () => {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
     canvas.width = cols * tileSize;
     canvas.height = rows * tileSize;
 };
-const initSnake = () => { // Setup snake with head at a random location.
+const initSnake = () => {
     snake = [];
-    snake.push(new Point(randomInt(0, cols - 1), randomInt(0, rows - 1)));
+    snake.push(new Point(Math.random() * cols | 0, Math.random() * rows | 0));
     dir = [4];
+};
+const loop = now => {
+    progress += (now - prev) / (1e3 / sps);
+    prev = now;
+    if (progress >= 1) {
+        progress--;
+        if (!paused && ! finalText) {
+            step();
+        }
+        draw();
+    }
+    requestAnimationFrame(loop);
 };
 const step = () => {
     if (dir.length > 1) {
@@ -92,62 +99,43 @@ const step = () => {
     }
 };
 const newApple = () => {
-    const possible = new Array(cols * rows);
-    for (let n = 0; n < possible.length; n++) {
-        possible[n] = n;
-    }
-    for (let n = 0; n < snake.length; n++) {
-        possible.splice(possible.indexOf(snake[n].y * cols + snake[n].x), 1);
-    }
-    const index = possible[randomInt(0, possible.length - 1)];
-    apple = new Point(index % cols, ~~(index / cols));
-};
-const randomInt = (min, max) => { // inclusive
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    let x;
+    let y;
+    do {
+        const index = Math.random() * rows * cols | 0;
+        x = index % cols;
+        y = index / cols | 0;
+    } while(snake.some(p => p.x === x && p.y === y));
+    apple = new Point(x, y);
 };
 const drawText = (string, x, y, maxWidth) => {
-    ctx.strokeStyle = "turquoise";
-    ctx.fillStyle = "black";
+    ctx.strokeStyle = hsl(Date.now() % 1e4 / 1e4);
+    ctx.fillStyle = "#000";
     ctx.fillText(string, x, y, maxWidth);
     ctx.strokeText(string, x, y, maxWidth);
 };
-const setFont = (string, size) => {
-    ctx.font = size + "px PressStart2P";
-    while (ctx.measureText(string).width + 20 > canvas.width) {
-        size--;
-        ctx.font = size + "px PressStart2P";
-    }
-    return size;
-};
+const hsl = percent => `hsl(${percent * 360}, 100%, 50%)`;
+const setFont = size => ctx.font = `${size}px PressStart2P, Trebuchet MS`;
 const draw = () => { // Draw entire frame.
-    ctx.fillStyle = "black"; // Clear canvas.
+    ctx.fillStyle = "#000"; // Clear canvas.
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath(); // Draw Apple.
-    ctx.fillStyle = "turquoise";
+    ctx.fillStyle = hsl(Date.now() % 5e3 / 5e3);
     ctx.arc((apple.x + 0.5) * tileSize, (apple.y + 0.5) * tileSize, tileSize / 2, 0, 2 * Math.PI);
     ctx.fill();
 
-    if (rainbowMode) {
-        for (let n = snake.length - 1; n >= 0; n--) { // Draw rainbow snake.
-            ctx.fillStyle = "hsl(" + ~~((n / snake.length) * 361) + ", 100%, 50%)";
-            ctx.fillRect((snake[(snake.length - 1) - n].x) * tileSize, (snake[(snake.length - 1) - n].y) * tileSize, tileSize, tileSize);
-        }
-    } else {
-        ctx.beginPath(); // Draw snake.
-        ctx.fillStyle = "turquoise";
-        for (let n = 0; n < snake.length - 1; n++) { // minus one, beacuase we don't want to draw the head.
-            ctx.moveTo(snake[n].x * tileSize, snake[n].y * tileSize);
-            ctx.rect(snake[n].x * tileSize, snake[n].y * tileSize, tileSize, tileSize);
-        }
-        ctx.fill();
-        // Draw snake's head.
-        ctx.fillStyle = "#00898e"; // Dark turquoise.
-        ctx.fillRect(snake[snake.length - 1].x * tileSize, snake[snake.length - 1].y * tileSize, tileSize, tileSize);
+    for (let n = 0; n < snake.length; n++) { // Draw rainbow snake.
+        const cur = snake[n];
+        const l = snake.length;
+        const a = 1e3 / sps;
+        const b = a * Math.max(l, 24);
+        ctx.fillStyle = hsl((Date.now() + n * a) % b / b);
+        ctx.fillRect(cur.x * tileSize, cur.y * tileSize, tileSize, tileSize);
     }
 
     ctx.beginPath(); // Draw grid.
-    ctx.strokeStyle = "white";
+    ctx.strokeStyle = "#fff";
     ctx.lineWidth = 0.5;
     for (let x = 0; x <= cols; x++) {
         ctx.moveTo(x * tileSize, 0);
@@ -159,44 +147,30 @@ const draw = () => { // Draw entire frame.
     }
     ctx.stroke();
 
-    if (paused) {
-        ctx.lineWidth = 2;
-        ctx.font = (canvas.width / 10) + "px PressStart2P";
-        let offset = (canvas.width - ctx.measureText("paused").width) / 2;
-        drawText("paused", offset, canvas.height / 2 + canvas.width / 32);
-        ctx.lineWidth = 1;
-        ctx.font = (canvas.width / 32) + "px PressStart2P";
-        offset = (canvas.width - ctx.measureText("press enter or space to resume").width) / 2;
-        drawText("press enter or space to resume", offset, canvas.height * 19 / 32);
+    ctx.textAlign = "center"; // main text
+    ctx.textBaseline = "middle";
+    ctx.lineWidth = 2;
+    const midX = canvas.width / 2;
+    const midY = canvas.height / 2;
+    if (paused || finalText) {
+        const text = finalText || "paused";
+        setFont(Math.min(midX / text.length * 1.5, midX / 5));
+        drawText(text, midX, midY);
+        setFont(midX / 16);
+        drawText("press enter or space to play", midX, midY * 1.15);
     }
 
-    if (finalText) { // Draw final text.
-        ctx.lineWidth = 2;
-        clearInterval(loop);
-        setFont(finalText, (canvas.width / 14));
-        let offset = ((canvas.width - ctx.measureText(finalText).width) / 2);
-        drawText(finalText, offset + 10, canvas.height / 2 + canvas.width / 32);
-
-        ctx.lineWidth = 1;
-        ctx.font = (canvas.width / 32) + "px PressStart2P";
-        offset = ((canvas.width - ctx.measureText("press enter or space to respawn").width) / 2);
-        drawText("press enter or space to respawn", offset, canvas.height * 6 / 10);
-    }
-
-    ctx.fillStyle = "white"; // Draw score.
-    ctx.font = tileSize * 0.6 + "px PressStart2P";
-    ctx.fillText("Score: " + (snake.length - 1), 5, canvas.height - 5);
+    ctx.fillStyle = "#fff"; // score
+    setFont(tileSize * 0.6);
+    ctx.textAlign = "left";
+    ctx.textBaseline = "bottom";
+    ctx.fillText(`Score: ${snake.length - 1}`, 5, canvas.height);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
     initCanvas();
     initSnake();
     newApple();
-    draw();
-    loop = setInterval(main = () => {
-        step();
-        draw();
-    }, 1000 / sps);
+    prev = performance.now();
+    requestAnimationFrame(loop);
 });
